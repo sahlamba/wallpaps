947398bbf1baea69efb64809f1a21338d3031595
thedrumsknight <sahil.lamba95@gmail.com>
2018-07-30 12:55:38 +0530

    [feat] Add setAsBackground feat, <CORS error, TODO: fetch api>

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { APP_WIDTH, APP_HEIGHT } from '../config';
 
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+import wallpaper from 'wallpaper';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -9,6 +14,8 @@ class App extends React.Component {
       randomSig: 0,
     };
     this.refreshImage = this.refreshImage.bind(this);
+    this.convertToBase64 = this.convertToBase64.bind(this);
+    this.setAsBackground = this.setAsBackground.bind(this);
   }
 
   componentWillMount() {
@@ -21,16 +28,46 @@ class App extends React.Component {
     });
   }
 
+  convertToBase64(imgSrc) {
+    const img = new Image();
+    img.onload = () => {
+      let canvas = document.createElement('canvas');
+      canvas.width = APP_WIDTH;
+      canvas.height = APP_HEIGHT;
+      let context = canvas.getContext('2d');
+      context.drawImage(img, 0, 0);
+      let dataURL = canvas.toDataURL('image/jpg');
+      return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
+    };
+    img.crossOrigin = 'anonymous';
+    img.src = imgSrc;
+  }
+
+  setAsBackground(event) {
+    let base64Image = this.convertToBase64(event.target.src);
+    let picturePath = path.join(os.homedir(), '/Pictures', 'wallpaps.jpg');
+    picturePath = path.normalize(picturePath);
+    fs.writeFile(picturePath, base64Image, 'base64', err => {
+      wallpaper.set(picturePath, { scale: 'stretch' }).then(() => {
+        console.log('Set background: ' + path.resolve(picturePath));
+      });
+    });
+  }
+
   render() {
     return (
       <div className="appContainer">
         <button className="refreshImage" onClick={this.refreshImage}>
           Refresh
         </button>
+        <p className="refreshImage setAsBackground">
+          Click on the image to set as background.
+        </p>
         <img
           src={`https://source.unsplash.com/random/${APP_WIDTH}x${APP_HEIGHT}?sig=${
             this.state.randomSig
           }`}
+          onClick={this.setAsBackground}
         />
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  target: 'node',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
